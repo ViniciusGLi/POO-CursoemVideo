564bce0ab3887ba9c63327d7edab7a124d9556b9
viniciusGli <vinicius.gutemberg1@gmail.com>
2023-05-28 13:17:03 -0300

    Métodos Getter, Setter and Contructor

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,15 @@
+
+package aula04;
+
+
+public class Aula04 {
+
+ 
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("BIC",0.5f,"azul");
+       // c1.setModelo("BIC");
+        //c1.setPonta(0.4f);
+        c1.status();
+    }
+    
+}
